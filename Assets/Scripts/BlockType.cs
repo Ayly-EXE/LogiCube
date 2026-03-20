e19ff0c7f64855c118488a0722dbbae3b268a331
Erwann Charlier <erwann.charlier@outlook.be>
2026-03-20 15:52:06 +0100

    save

--- Assets/Scripts/BlockType.cs
+++ Assets/Scripts/BlockType.cs
@@ -3,7 +3,7 @@ public enum BlockType
     Air = 0,  // vide
     Dirt = 1,  // terre
     Stone = 2,  // pierre
-    Grass =3,
+    Grass = 3,  // herbe
 }
 
 public static class BlockTypeExtensions
